05edf161819eb72d927479ca1972e3391086b0e6
chase coalwell <c.coalwell@gmail.com>
2011-10-11 20:29:29 -0700

    Test fails and you're my only hope.

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -1,24 +1,41 @@
 describe "Strings" do
   context "when calling strip" do
 
-    it "should remove all whitespace from the beginning and the end of the string"
+    it "should remove all whitespace from the beginning and the end of the string" do
 
         lyrics = "  Hello, is it me you're looking for I can see it in your eyes  "
 
         lyrics.strip.should eq "Hello, is it me you're looking for I can see it in your eyes"
+    end
+
+  end
+
+end
+
+describe "Strings" do
+  context "when calling swapcase" do
+    
+    it "should change the switch the case of each character" do
+
+      phrase = "The rain in Spain falls mainly on the plain"
+
+      phrase.swapcase.should eq "THE RAIN IN SPAIN FALLS MAINLY ON THE PLAIN"
+
+    end
 
   end
 
 end
 
 describe "Integer" do
-  context "when calling even" do
+  context "when calling odd" do
 
-    it "should indicate if a given integer is even"
+    it "should indicate if a given integer is odd" do
 
 	a_number = 4
 
-	a_number.odd?.should eq true
-
+	a_number.odd?.should be true
+    end
   end
 end
+
